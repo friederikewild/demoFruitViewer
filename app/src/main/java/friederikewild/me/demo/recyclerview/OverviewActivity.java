3c1bfa9976e098c73ad19259f3f96b5d41ed8149
Friederike Wild <dev@friederikewild.me>
2018-02-01 14:52:28 +0000

    * Initial Android project with blank activity created via Android Studio

---
+++ app/src/main/java/friederikewild/me/demo/recyclerview/OverviewActivity.java
@@ -0,0 +1,15 @@
+package friederikewild.me.demo.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverviewActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
