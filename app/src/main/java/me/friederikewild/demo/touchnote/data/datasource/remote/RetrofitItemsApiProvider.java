8514b3df865c704e8f98c821f99d42a4e16caee7
Friederike Wild <dev@friederikewild.me>
2018-02-05 16:56:28 +0000

    * Store unused code as todo for possible network caching using okhttp client interceptor

--- app/src/main/java/me/friederikewild/demo/touchnote/data/datasource/remote/RetrofitItemsApiProvider.java
+++ app/src/main/java/me/friederikewild/demo/touchnote/data/datasource/remote/RetrofitItemsApiProvider.java
@@ -1,14 +1,18 @@
 package me.friederikewild.demo.touchnote.data.datasource.remote;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 
+import java.io.File;
 import java.util.Collection;
 
 import me.friederikewild.demo.touchnote.BuildConfig;
 import me.friederikewild.demo.touchnote.data.GetNoDataCallback;
 import me.friederikewild.demo.touchnote.data.datasource.ItemsDataStore;
 import me.friederikewild.demo.touchnote.data.entity.ItemEntity;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,6 +59,9 @@ public class RetrofitItemsApiProvider implements ItemsApiProvider
             httpClientBuilder.addInterceptor(loggingInterceptor);
         }
 
+        // TODO: Something to explore further. Could enhance user experience
+//        enableOkHttpCaching(httpClientBuilder);
+
         final OkHttpClient httpClient = httpClientBuilder.build();
 
         // Setup retrofitClient builder
@@ -65,6 +72,36 @@ public class RetrofitItemsApiProvider implements ItemsApiProvider
 
         // Create retrofitClient
         return retrofitBuilder.build();
+    }
+
+    /**
+     * Something to experiment with in the future.
+     * Needs proper handling of providing {@link Context#getCacheDir} and {@link #isNetworkAvailable}.
+     * Both could be wrapped and provided via injections.
+     *
+     * @param httpClientBuilder The created and not yet used http client builder
+     * @param cacheDir          Link to the context cache dir
+     */
+    @SuppressWarnings("Unused")
+    private void enableOkHttpCaching(@NonNull OkHttpClient.Builder httpClientBuilder, @NonNull File cacheDir)
+    {
+        long maxSize = 10 * 1024 * 1024; // 10 MB
+        httpClientBuilder.cache(new Cache(cacheDir, maxSize))
+                .addInterceptor(chain ->
+                                {
+                                    Request request = chain.request();
+                                    if (isNetworkAvailable())
+                                    {
+                                        request = request.newBuilder().header("Cache-Control",
+                                                                              "public, max-age=" + 60).build();
+                                    }
+                                    else
+                                    {
+                                        request = request.newBuilder().header("Cache-Control",
+                                                                              "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build();
+                                    }
+                                    return chain.proceed(request);
+                                });
     }
 
     @NonNull
@@ -138,5 +175,18 @@ public class RetrofitItemsApiProvider implements ItemsApiProvider
         {
             errorCallback.onNoDataAvailable();
         }
+    }
+
+    private static boolean isNetworkAvailable()
+    {
+        /*
+        public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+                */
+        return true;
     }
 }
